03ad1ed93eae4fb06f1cd2f181b9d3a71d60e905
Nick Golikov <nick@exads.com>
2018-06-01 17:13:42 +0100

    release_candidate update fluid player version

--- fluidplayer.js
+++ fluidplayer.js
@@ -109,7 +109,7 @@ var fluidPlayerClass = {
         'toggleElementText', 'getMobileOs', 'findClosestParent', 'activeVideoPlayerId',
         'getInstanceIdByWrapperId', 'timer', 'timerPool', 'adList', 'adPool',
         'isUserActive', 'isCurrentlyPlayingAd', 'initialAnimationSet'],
-    version: '2.2.2',
+    version: '2.3.0',
     homepage: 'https://www.fluidplayer.com/',
     activeVideoPlayerId: null,
 
