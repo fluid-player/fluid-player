1b240872c51e24065b2aeacb0cb178396d9f71b7
max <max@max.lan>
2020-05-21 14:25:21 +0200

    static thumbnail configuration

--- src/modules/timeline.js
+++ src/modules/timeline.js
@@ -163,35 +163,39 @@ export default function (playerInstance, options) {
     };
 
     playerInstance.setupThumbnailPreview = () => {
-        if (playerInstance.displayOptions.layoutControls.timelinePreview &&
-            (typeof playerInstance.displayOptions.layoutControls.timelinePreview.file === 'string') &&
-            (typeof playerInstance.displayOptions.layoutControls.timelinePreview.type === 'string')) {
+        const timelinePreview = playerInstance.displayOptions.layoutControls.timelinePreview;
+        if (timelinePreview &&
+            ((timelinePreview.type === 'VTT' && typeof timelinePreview.file === 'string') ||
+             (timelinePreview.type === 'static' && typeof timelinePreview.frames === 'object')) ) {
+            let eventOn = 'mousemove';
+            let eventOff = 'mouseleave';
+            if (playerInstance.mobileInfo.userOs) {
+                eventOn = 'touchmove';
+                eventOff = 'touchend';
+            }
+            document.getElementById(playerInstance.videoPlayerId + '_fluid_controls_progress_container')
+                .addEventListener(eventOn, playerInstance.drawTimelinePreview.bind(playerInstance), false);
+            document.getElementById(playerInstance.videoPlayerId + '_fluid_controls_progress_container')
+                .addEventListener(eventOff, function (event) {
+                    const progress = document.getElementById(playerInstance.videoPlayerId + '_fluid_controls_progress_container');
+                    if (typeof event.clientX !== 'undefined' && progress.contains(document.elementFromPoint(event.clientX, event.clientY))) {
+                        //False positive (Chrome bug when fast click causes leave event)
+                        return;
+                    }
+                    document.getElementById(playerInstance.videoPlayerId + '_fluid_timeline_preview_container').style.display = 'none';
+                    document.getElementById(playerInstance.videoPlayerId + '_fluid_timeline_preview_container_shadow').style.display = 'none';
+                }, false);
+            playerInstance.generateTimelinePreviewTags();
 
-            if (playerInstance.displayOptions.layoutControls.timelinePreview.type === 'VTT') {
-                let eventOn = 'mousemove';
-                let eventOff = 'mouseleave';
-                if (playerInstance.mobileInfo.userOs) {
-                    eventOn = 'touchmove';
-                    eventOff = 'touchend';
-                }
-                document.getElementById(playerInstance.videoPlayerId + '_fluid_controls_progress_container')
-                    .addEventListener(eventOn, playerInstance.drawTimelinePreview.bind(playerInstance), false);
-                document.getElementById(playerInstance.videoPlayerId + '_fluid_controls_progress_container')
-                    .addEventListener(eventOff, function (event) {
-                        const progress = document.getElementById(playerInstance.videoPlayerId + '_fluid_controls_progress_container');
-                        if (typeof event.clientX !== 'undefined' && progress.contains(document.elementFromPoint(event.clientX, event.clientY))) {
-                            //False positive (Chrome bug when fast click causes leave event)
-                            return;
-                        }
-                        document.getElementById(playerInstance.videoPlayerId + '_fluid_timeline_preview_container').style.display = 'none';
-                        document.getElementById(playerInstance.videoPlayerId + '_fluid_timeline_preview_container_shadow').style.display = 'none';
-                    }, false);
-                playerInstance.generateTimelinePreviewTags();
-
+            if (timelinePreview.type === 'VTT') {
                 import(/* webpackChunkName: "webvtt" */ '../../vendor/webvtt').then((it) => {
                     window.WebVTTParser = it.default;
                     playerInstance.setupThumbnailPreviewVtt();
                 });
+            }
+            else {
+                playerInstance.displayOptions.layoutControls.timelinePreview.spriteImage = true;
+                playerInstance.timelinePreviewData = timelinePreview.frames;
             }
 
             playerInstance.showTimeOnHover = false;
