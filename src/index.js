b21933d6dfa69941989d7bfe8790e31aed388ac7
M. Bennett <illogic-al@users.noreply.github.com>
2021-11-19 05:32:17 -0500

    Add check for process being defined.
    Fixes 'process not defined' error when using with GatsbyJS.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ if ('undefined' === typeof FP_ENV) {
             || window.location.hostname === '127.0.0.1'
             || window.location.hostname === '');
 
-    if (process && process.env && process.env.NODE_ENV) {
+    if ('undefined' !== typeof process && process && process.env && process.env.NODE_ENV) {
         global.FP_ENV = process.env.NODE_ENV;
     } else if (window && !isLocalhost) {
         global.FP_ENV = 'production';
