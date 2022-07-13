e2f4fed15183850450cdb8e0c690c88d6daa5523
Leroy <lleroy.m@gmail.com>
2022-07-13 17:37:24 +0100

    fix: code review

--- src/modules/vast.js
+++ src/modules/vast.js
@@ -24,7 +24,7 @@ export default function (playerInstance, options) {
                         mobileText || desktopText :
                         desktopText || mobileText,
                     link: link || null,
-                    tracking: tracking || link,
+                    tracking,
                 }
             }
         }
