1dea38c8fdaf19968225e2c3ef8ed5ca4bf0b52c
danielapereira11 <brunadaniela.pereira.27@gmail.com>
2025-07-29 17:03:10 +0200

    feature: add a constant file

---
+++ src/constants/constants.js
@@ -0,0 +1,54 @@
+export const trackingEventTypes = {
+    // Player Operation Metrics (use in linear and non-linear ads)
+    muteEvent: 'mute',
+    unmuteEvent: 'unmute',
+    pauseEvent: 'pause',
+    resumeEvent: 'resume',
+    rewindEvent: 'rewind',
+    fullscreenEvent: 'fullscreen',
+    skipEvent: 'skip',
+    playerExpandEvent: 'playerExpand',
+    playerCollapseEvent: 'playerCollapse',
+    impressionEvent: 'impression',
+    viewImpressionEvent: 'viewImpression',
+
+    // Use in linear ad metrics
+    startEvent: 'start',
+    firstQuartileEvent: 'firstQuartile',
+    midpointEvent: 'midpoint',
+    thirdQuartileEvent: 'thirdQuartile',
+    completeEvent: 'complete',
+    progressEvent: 'progress',
+
+    // Use in non-linear ad metrics
+    creativeViewEvent: 'creativeView',
+    collapseEvent: 'collapse',
+    adCollapseEvent: 'adCollapse',
+    closeEvent: 'close',
+    acceptInvitationEvent: 'acceptInvitation',
+
+    iconClickThroughEvent: 'iconClickThrough',
+}
+
+export const displayModes = {
+    FULLSCREEN: 'fullScreen',
+    THEATER: 'theaterMode',
+    MINI_PLAYER: 'miniPlayer',
+    NORMAL: 'normal',
+}
+
+export const vastTags = {
+    inLineTag: 'InLine',
+    linearTag: 'Linear',
+    nonLinearTag: 'NonLinear',
+    videoClicksTag: 'VideoClicks',
+    clickThroughTag: 'ClickThrough',
+    clickTrackingTag: 'ClickTracking',
+    customClickTag: 'CustomClick',
+    nonLinearClickTrackingTag: 'NonLinearClickTracking',
+    nonLinearClickThroughTag: 'NonLinearClickThrough',
+    impressionTag: 'Impression',
+    viewableTag: 'Viewable',
+    errorTag: 'Error',
+    titleCtaTag: 'TitleCTA',
+}
